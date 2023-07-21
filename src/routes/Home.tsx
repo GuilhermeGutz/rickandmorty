001fd8b5a527d623c82c8e55b80eaf02f6d18c75
GuilhermeGutz <guilhermegutz6@gmail.com>
2023-07-20 22:34:33 -0300

    add search bar add grid from material ui

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,13 @@
-import { Button, styled } from '@mui/material'
+import { Button, TextField, Container, Grid, styled, Box } from '@mui/material'
 import logo from '../image/background/rickandmortyt1.svg'
+import background from '../image/background/backgroundt1.svg'
 import './Home.css'
 
 const StyledButton = styled(Button)(() =>({
   width: "84px",
   height: "42px",
   color: "#ffffff",
+  textTransform: "none",
   borderRadius: "8px",
   border: "2px solid #ffffff",
   "&:hover":{
@@ -14,12 +16,47 @@ const StyledButton = styled(Button)(() =>({
   }
 }))
 
+const StyledTextField = styled(TextField)(() =>({
+  width: "px",
+  height: "42px",
+  color: "#9F9F9F",
+ //borderRadius: "8px",
+ //border: "2px solid #ffffff",
+  "&:hover":{
+    backgroundColor: "grey",
+   // border: "3px solid #1E9201",
+    borderColor: "#1E9201",
+  }
+}))
+
+const StyledContainer = styled(Container)(() =>({
+  width: "100vw",
+  height: "100vh",
+  //backgroundPosition: "center",
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  position: "fixed",
+  left: "0",
+  top: "0",
+  padding: 0,
+}))
+
 function Home() {
   return (
-    <div className='container'>
-      <img src={logo} alt="logo"/>
-      <StyledButton variant='outlined'>Search</StyledButton> 
-    </div>
+    <StyledContainer>
+      <Grid container rowSpacing={6} columnSpacing={6}>
+        <Grid item xs={12}>
+          <img src={logo} alt="logo"/>
+        </Grid>
+        <Grid item xs={8}>
+          <StyledTextField label="Search characters" variant="outlined"/>
+        </Grid>
+        <Grid item xs={0}>
+          <StyledButton variant='outlined'>Search</StyledButton>
+        </Grid>
+      </Grid>
+    </StyledContainer>
   )
 }
 
